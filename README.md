9bfe8319513fa4239a0c275c1c5dd5decea26bd4
wojciechiwanicki <wojciech.iwanicki@laboratorium.ee>
2017-09-30 19:38:38 +0200

    editing and adding customers option added, minor structural changes and first attempts to style whole thing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-this is a simple application in react, that uses NyKredit hackaton HAL api. It aims at dispalying existing ad adding new customers. 
+this is a simple application in react, that uses NyKredit hackaton HAL api. It aims at dispalying existing and adding new customers.
 to run this project:
 1) clone repo
 2) ``cd react_hal_api``
--- src/resources/customers.js
+++ src/resources/customers.js
@@ -1,40 +1,37 @@
 import React, {Component} from 'react';
 
-//const authUrl = 'https://i1.test-services.nykredit.it/security/oauth2/token?grant_type=client_credentials';
 const url = 'https://i1.test-services.nykredit.it/cem-hackathon-service/customers/';
-
 class Customers extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            input: ''
+            auth: '',
+            input: {
+                cprCvr: '',
+                firstName: '',
+                lastName: ''
+            }
         }
         this.handleSubmit = this.handleSubmit.bind(this);
-        //this.handleSubmit = this.handleSubmit.bind(this);
     }
-    /*componentWillMount() {
-        fetch(authUrl, {
-            method: "POST",
-            headers: {
-                "Authorization": "Basic dGVzdC1jbGllbnRpZDpwYXNzd29yZA=="
-            }
-        }).then(response => response.json())
-        .then(jsondata => this.setState({auth_data: jsondata}))
 
-    }*/
     componentDidMount() {
-        setTimeout(() => {
-            fetch(url, {
-                method: "GET",
-                headers: {
-                    "Accept": "application/hal+json",
-                    "X-Client-Version": "1.0.0",
-                    "Authorization": "Bearer YjQ5YjU1ZjMtOTA3ZC00ZDdiLWEwNGEtNGRjOWVmMGI1YmQ1"
-                }
-            }).then(response => response.json()).then(jsondata => this.setState({data: jsondata}))
-        }, 500)
-    }
-    
+            const options = {
+            method: 'GET',
+            headers: {
+                "Accept": "application/hal+json",
+                "X-Client-Version": "1.0.0",
+                "Authorization": "Bearer MGI3ZTc2NDAtZTc0Yi00YzY5LTg0OGYtNmJiYjUzMTNjNTRk"
+            }
+        }
+            fetch(url, options)
+            .then(response => response.json())
+            .then(jsondata => this.setState({data: jsondata}))
+            .catch(function() {
+                alert('There was something wrong. Data could not be fetched :(((')
+            });
+        }
+
     handleChangeFor = (propertyName) => (event) => {
         const {input} = this.state;
         const newInput = {
@@ -45,25 +42,34 @@ class Customers extends Component {
     }
 
     handleSubmit(event) {
-        alert('NAME: ' + this.state.input.firstName);
-        console.log('INPUT WORKS!');
+        const options = {
+            method: 'PUT',
+            headers: {
+                "Accept": "application/hal+json",
+                "X-Client-Version": "1.0.0",
+                "Authorization": "Bearer MGI3ZTc2NDAtZTc0Yi00YzY5LTg0OGYtNmJiYjUzMTNjNTRk",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.state.input)
+        }
+        fetch(url + this.state.input.cprCvr, options)
+            .then(response => response.json())
+            .then(console.log(this.state.input))
         event.preventDefault();
     }
+
     createList() {
         return this.state.data._embedded.customers.map((customer) => {
-            return (
-                <div key={customer.cprCvr}>
-                    <div>{customer.firstName}</div>
-                    <div>{customer.lastName}</div>
-                    <div>{customer.cprCvr}</div>
-                    <hr/>
-                </div>
-
+        return (
+                    <div key={customer.cprCvr} className='col s6 m4 l2 customer_cell'>
+                        <div className='name'>{customer.firstName}</div>
+                        <div className='surname'>{customer.lastName}</div>
+                        <div className='cprCvr'>{customer.cprCvr}</div>
+                    </div>
             )
         });
     }
     render() {
-
         if (!this.state.data)
             return (
                 <div>
@@ -72,26 +78,29 @@ class Customers extends Component {
             )
         return (
             <div>
+                <h4>Customers:</h4>
+                <div className='row'>
                 {this.createList()}
-
-                <form onSubmit={this.handleSubmit}>
+                </div>
+                <h4>Add/edit a customer</h4>
+                <form onSubmit={this.handleSubmit} className='add_new_customer'>
+                    <label>
+                        cprCvr:
+                        <input type="number" onChange={this.handleChangeFor('cprCvr')} value={this.state.input.cprCvr} required/>
+                    </label>
                     <label>
                         Name:
-                        <input type="text" onChange={this.handleChangeFor('firstName')} value={this.state.input.firstName}/>
+                        <input type="text" onChange={this.handleChangeFor('firstName')} value={this.state.input.firstName} required/>
                     </label>
                     <label>
                         Surname:
-                        <input type="text" onChange={this.handleChangeFor('lastName')} value={this.state.input.lastName}/>
+                        <input type="text" onChange={this.handleChangeFor('lastName')} value={this.state.input.lastName} required/>
                     </label>
-                    <label>
-                        cprCvr:
-                        <input type="number" onChange={this.handleChangeFor('cprCvr')} value={this.state.input.cprCvr}/>
-                    </label>
+
                     <input type="submit" value="Submit"/>
                 </form>
             </div>
         );
-
     }
 }
 
