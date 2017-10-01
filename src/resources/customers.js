f7e7d01a237a45106131b57cb3b388f33931f9d9
wojciechiwanicki <wojciech.iwanicki@laboratorium.ee>
2017-10-01 21:10:40 +0200

    button and borders plus some beautifying of code

--- src/resources/customers.js
+++ src/resources/customers.js
@@ -5,7 +5,8 @@ class Customers extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            auth: '',
+            isSubmitted: false,
+            auth_token: '/*here goes token*/',
             input: {
                 cprCvr: '',
                 firstName: '',
@@ -16,21 +17,22 @@ class Customers extends Component {
     }
 
     componentDidMount() {
-            const options = {
+        const options = {
             method: 'GET',
             headers: {
                 "Accept": "application/hal+json",
                 "X-Client-Version": "1.0.0",
-                "Authorization": "Bearer MGI3ZTc2NDAtZTc0Yi00YzY5LTg0OGYtNmJiYjUzMTNjNTRk"
+                "Authorization": "Bearer " + this.state.auth_token
             }
         }
-            fetch(url, options)
-            .then(response => response.json())
-            .then(jsondata => this.setState({data: jsondata}))
-            .catch(function() {
-                alert('There was something wrong. Data could not be fetched :(((')
-            });
-        }
+        fetch(url, options)
+        .then(response => response.json())
+        .then(jsondata => this.setState({data: jsondata}))
+        .catch(function() {
+            alert(
+                'There was something wrong. Data could not be fetched :(((')
+        });
+    }
 
     handleChangeFor = (propertyName) => (event) => {
         const {input} = this.state;
@@ -47,25 +49,27 @@ class Customers extends Component {
             headers: {
                 "Accept": "application/hal+json",
                 "X-Client-Version": "1.0.0",
-                "Authorization": "Bearer MGI3ZTc2NDAtZTc0Yi00YzY5LTg0OGYtNmJiYjUzMTNjNTRk",
+                "Authorization": "Bearer " + this.state.auth_token,
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(this.state.input)
         }
         fetch(url + this.state.input.cprCvr, options)
-            .then(response => response.json())
-            .then(console.log(this.state.input))
+        .then(response => response.json())
+        .then(jsondata => {
+            this.setState({isSubmitted: true})
+        })
         event.preventDefault();
     }
 
     createList() {
         return this.state.data._embedded.customers.map((customer) => {
-        return (
-                    <div key={customer.cprCvr} className='col s6 m4 l2 customer_cell'>
-                        <div className='name'>{customer.firstName}</div>
-                        <div className='surname'>{customer.lastName}</div>
-                        <div className='cprCvr'>{customer.cprCvr}</div>
-                    </div>
+            return (
+                <div key={customer.cprCvr} className='row customer_row'>
+                    <div className='col s4 m4 l4 customer_cell'>{customer.firstName}</div>
+                    <div className='col s4 m4 l4 customer_cell'>{customer.lastName}</div>
+                    <div className='col s4 m4 l4 customer_cell'>{customer.cprCvr}</div>
+                </div>
             )
         });
     }
@@ -77,27 +81,33 @@ class Customers extends Component {
                 </div>
             )
         return (
-            <div>
+            <div className='app_container container'>
                 <h4>Customers:</h4>
                 <div className='row'>
-                {this.createList()}
+                    <div className='row customers_legend'>
+                        <div className='col s4 m4 l4'>First Name</div>
+                        <div className='col s4 m4 l4'>Last Name</div>
+                        <div className='col s4 m4 l4'>CPR CVR</div>
+                    </div>
+                    {this.createList()}
                 </div>
                 <h4>Add/edit a customer</h4>
-                <form onSubmit={this.handleSubmit} className='add_new_customer'>
+                <p>To edit customer, type his/hers CPR CVR and new name and surname.</p>
+                <form onSubmit={this.handleSubmit} className='input_fields_container'>
                     <label>
                         cprCvr:
-                        <input type="number" onChange={this.handleChangeFor('cprCvr')} value={this.state.input.cprCvr} required/>
+                        <input type='number' onChange={this.handleChangeFor('cprCvr')} value={this.state.input.cprCvr} required/>
                     </label>
                     <label>
                         Name:
-                        <input type="text" onChange={this.handleChangeFor('firstName')} value={this.state.input.firstName} required/>
+                        <input type='text' onChange={this.handleChangeFor('firstName')} value={this.state.input.firstName} required/>
                     </label>
                     <label>
                         Surname:
-                        <input type="text" onChange={this.handleChangeFor('lastName')} value={this.state.input.lastName} required/>
+                        <input type='text' onChange={this.handleChangeFor('lastName')} value={this.state.input.lastName}/>
                     </label>
 
-                    <input type="submit" value="Submit"/>
+                    <input type='submit' value='Submit' className='submit_button_centered'/>
                 </form>
             </div>
         );
